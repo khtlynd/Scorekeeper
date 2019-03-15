1167a90d77d78a351032b22d47acb3e34b761fb4
khtlynd <khtlyndella@gmail.com>
2019-03-15 14:54:44 +0700

    Drawables, styles, adn themes

--- app/src/main/java/com/danyhp/mmithb/scorekeeper/MainActivity.java
+++ app/src/main/java/com/danyhp/mmithb/scorekeeper/MainActivity.java
@@ -2,12 +2,53 @@ package com.danyhp.mmithb.scorekeeper;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int mScore1;
+    private int mScore2;
+
+    private int mScoreText1;
+    private int mScoreText2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mScoreText1 = (TextView)findViewById(R.id.score_1);
+        mScoreText2 = (TextView)findViewById(R.id.score_2);
+    }
+
+    public void decreaseScore(View view) {
+        //get the ID of the button that was clicked
+        int viewID = view.getId();
+        switch (viewID){
+            //ON TEAM 1
+            case R.id.decreaseTeam1:
+                mScore1--;
+                mScoreText1.setText(String.valueOf(mScore1));
+                break;
+            case R.id.decreaseTeam2:
+                mScore2--;
+                mScoreText2.setText(String.valueOf(mScore2));
+        }
+    }
+
+    public void increaseScore(View view) {
+        //get the ID of the button that was clicked
+        int viewID = view.getId();
+        switch (viewID){
+            //ON TEAM 1
+            case R.id.increaseTeam1:
+                mScore1++;
+                mScoreText1.setText(String.valueOf(mScore1));
+                break;
+            case R.id.increaseTeam2:
+                mScore2++;
+                mScoreText2.setText(String.valueOf(mScore2));
+        }
     }
 }
